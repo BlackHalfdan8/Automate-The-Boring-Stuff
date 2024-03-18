5336acb5c48976b7df4ac7aff860315ca7fd023a
BlackHalfdan8 <141372947+BlackHalfdan8@users.noreply.github.com>
2024-03-17 21:51:51 -0400

    Update customPrettyCharacterCount.py
    Previous version had issues with the user signaling they were finished entering text.  New version permits a user to enter 'done' on a new line to signal they are finished.
    Removed the sys module since it is not called in this version.

--- chapter05/customPrettyCharacterCount.py
+++ chapter05/customPrettyCharacterCount.py
@@ -1,10 +1,14 @@
 import pprint
-import sys
 
-print('Enter the text for which you would like to count characters:')
-message = sys.stdin.read()
+print('Enter the text for which you would like to count characters. Type "done" on a new line when finished:')
+message = ''
+while True:
+    line = input()
+    if line.strip().lower() == 'done':
+        break
+    message += line + '\n'
+
 count = {}
-
 for character in message:
     count.setdefault(character, 0)
     count[character] += 1
