0318380e14dbd05f469f4d0168372990210ade8f
BlackHalfdan8 <141372947+BlackHalfdan8@users.noreply.github.com>
2024-07-06 00:52:56 -0400

    Update debuggingCoinToss.py
    debuggingCoinToss.py end of Chapter 11 practice project of Automate the Boring Stuff with Python after debugging code.

--- chapter11/debuggingCoinToss.py
+++ chapter11/debuggingCoinToss.py
@@ -3,14 +3,19 @@ import random
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
-    guess = input()
+    guess = input().lower()
+
 toss = random.randint(0, 1)  # 0 is tails, 1 is heads
-if toss == guess:
+
+# Convert the toss result to 'heads' or 'tails'
+toss_result = 'heads' if toss == 1 else 'tails'
+
+if toss_result == guess:
     print('You got it!')
 else:
-    print('Nope! Guess again!')
-    guess = input()
-    if toss == guess:
+    print('Nope! Guess again! Enter heads or tails:')
+    guess = input().lower()
+    if toss_result == guess:
         print('You got it!')
     else:
         print('Nope. You are really bad at this game.')
