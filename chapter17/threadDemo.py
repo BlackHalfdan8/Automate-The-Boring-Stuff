0275f933dc503fb688b697e985cff2944a5d47f2
BlackHalfdan8 <141372947+BlackHalfdan8@users.noreply.github.com>
2024-07-11 16:53:05 -0400

    Update threadDemo.py
    corrected line 8 typo from ...=takeANamp to =takeANap so the program executes correctly

--- chapter17/threadDemo.py
+++ chapter17/threadDemo.py
@@ -5,7 +5,7 @@ def takeANap():
     time.sleep(5)
     print('Wake up!')
     
-threadObj = threading.Thread(target=takeANamp)
+threadObj = threading.Thread(target=takeANap)
 threadObj.start()
 
 print('End of program.')
