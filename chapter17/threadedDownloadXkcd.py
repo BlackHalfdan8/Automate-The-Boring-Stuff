d15f81c4f6e4536572f304105b4d97fbc8f54a0c
BlackHalfdan8 <141372947+BlackHalfdan8@users.noreply.github.com>
2024-07-11 17:12:21 -0400

    Update threadedDownloadXkcd.py
    Improved handling of "comicUrl" to ensure it includes the protocol.  Changed the loop range to 'range(startComic, endComic + 1)' to include the last comic in the range.
    Added error handling to catch and print any 'requests' exceptions during the download process.
    used a context manager ('with' statement) for opening and writing the image file to ensure proper closure.

--- chapter17/threadedDownloadXkcd.py
+++ chapter17/threadedDownloadXkcd.py
@@ -2,41 +2,48 @@
 # threadedDownloadXkcd.py - Downloads XKCD comics using multiple threads.
 
 import requests, os, bs4, threading
-os.makedirs('xkcd', exist_ok=True)      # store comics in ./xkcd
+
+os.makedirs('xkcd', exist_ok=True)  # Store comics in ./xkcd
 
 def downloadXkcd(startComic, endComic):
-    for urlNumber in range(startComic, endComic):
-        # Download the page.
-        print('Downloading page https://xkcd.com/%s...' % (urlNumber))
-        res = requests.get('https://xkcd.com/%s' % (urlNumber))
-        res.raise_for_status()
-        
-        soup = bs4.BeautifulSoup(res.text, 'html.parser')
-        
-        # Find the URL of the comic image.
-        comicElem = soup.select('#comic img')
-        if comicElem == []:
-            print('Could not find comic image.')
-        else:
+    for urlNumber in range(startComic, endComic + 1):
+        try:
+            # Download the page.
+            print('Downloading page https://xkcd.com/%s...' % (urlNumber))
+            res = requests.get('https://xkcd.com/%s' % (urlNumber))
+            res.raise_for_status()
+            
+            soup = bs4.BeautifulSoup(res.text, 'html.parser')
+            
+            # Find the URL of the comic image.
+            comicElem = soup.select('#comic img')
+            if comicElem == []:
+                print('Could not find comic image.')
+                continue
             comicUrl = comicElem[0].get('src')
+            if not comicUrl.startswith('http'):
+                comicUrl = 'https:' + comicUrl
+            
             # Download the image.
             print('Downloading image %s...' % (comicUrl))
-            res = requests.get('https:' + comicUrl)
+            res = requests.get(comicUrl)
             res.raise_for_status()
             
             # Save the image to ./xkcd
-            imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
-            for chunk in res.iter_content(100000):
-                imageFile.write(chunk)
-            imageFile.close()
-        
+            imageFilePath = os.path.join('xkcd', os.path.basename(comicUrl))
+            with open(imageFilePath, 'wb') as imageFile:
+                for chunk in res.iter_content(100000):
+                    imageFile.write(chunk)
+        except requests.exceptions.RequestException as e:
+            print(f'Error downloading {urlNumber}: {e}')
+
 # Create and start the Thread objects.
-downloadThreads = []                    # a list of all the Thread objects
-for i in range(0, 140, 10):             # loops 14 times, creates 14 threads
+downloadThreads = []  # A list of all the Thread objects
+for i in range(0, 140, 10):  # Loops 14 times, creates 14 threads
     start = i
     end = i + 9
     if start == 0:
-        start = 1                       # There is no comic 0, so set it to 1.
+        start = 1  # There is no comic 0, so set it to 1.
     downloadThread = threading.Thread(target=downloadXkcd, args=(start, end))
     downloadThreads.append(downloadThread)
     downloadThread.start()
